085c572bb36ae86210e019e2891029827c0141bc
Usam Sersultanov <usam97@mail.ru>
2020-06-07 17:17:03 +0200

    add store and load options for parameters of trained network

--- agent.py
+++ agent.py
@@ -3,7 +3,7 @@ import random
 from collections import namedtuple, deque 
 
 from model import Network 
-
+import os
 import torch 
 import torch.nn as nn 
 import torch.nn.functional as F 
@@ -13,8 +13,8 @@ from constants import *
 
 class Agent():
     """Interacts with and learns from the environment."""
-	
-	def __init__(self, state_size, action_size, seed):
+    
+    def __init__(self, state_size, action_size, seed):
         """Initialize an Agent object.
 
         Params
@@ -31,7 +31,7 @@ class Agent():
         # Q-Network 
         self.qnetwork_local  = Network(state_size, action_size, seed).to(device)
         self.qnetwork_target = Network(state_size, action_size, seed).to(device)
-        self.optimizer 		 = optim.Adam(self.qnetwork_local.parameters(), ls=LR)
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
         
         # Replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
@@ -40,15 +40,15 @@ class Agent():
 
     def step(self, state, action, reward, next_state, done):
         # Save experience in replay memory    	
-    	self.memory.add(state, action, reward, next_state, done)
+        self.memory.add(state, action, reward, next_state, done)
 
         # Learn every UPDATE_EVERY time steps.
         self.t_step = (self.t_step + 1) % UPDATE_EVERY
         if self.t_step == 0: 
-        	# If enough samples are available in memory, get random subset and learn
-        	if len(self.memory) > BATCH_SIZE:
-        		experiences = self.memory.sample()
-        		self.learn(experiences, GAMMA)
+            # If enough samples are available in memory, get random subset and learn
+            if len(self.memory) > BATCH_SIZE:
+                experiences = self.memory.sample()
+                self.learn(experiences, GAMMA)
 
     def act(self, state, eps=0.):
         """Returns actions for given state as per current policy.
@@ -61,17 +61,17 @@ class Agent():
         state = torch.from_numpy(state).float().unsqueeze(0).to(device)
         self.qnetwork_local.eval()
         with torch.no_grad():
-        	action_values = self.qnetwork_local(state)
+            action_values = self.qnetwork_local(state)
 
         self.qnetwork_local.train()
 
         # Epsilon-greedy action selection
-		if random.random() > eps: 
-			return np.armgax(action_values.cpu().data.numpy())
-		else: 
-			return random.choice(np.arange(self.action_size))
+        if random.random() > eps: 
+            return np.argmax(action_values.cpu().data.numpy())
+        else: 
+            return random.choice(np.arange(self.action_size))
 
-	def learn(self, experiences, gamma):
+    def learn(self, experiences, gamma):
         """Update value parameters using given batch of experience tuples.
 
         Params
@@ -82,9 +82,9 @@ class Agent():
         states, actions, rewards, next_states, dones = experiences
 
         # Get max predicted Q values (for next states) from target model
-		Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
-		# Compute Q targets for current states 
-		Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        # Compute Q targets for current states 
+        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
         # Get expected Q values from local model
         Q_expected = self.qnetwork_local(states).gather(1, actions)
@@ -111,6 +111,20 @@ class Agent():
         """
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
+
+    def store(self):
+        torch.save({'state_dict' : self.qnetwork_local.state_dict(),
+                    'optimizer'  : self.optimizer.state_dict(),}, 'last_brain.pth')
+
+    def load(self):
+        if os.path.isfile('last_brain.pth'):
+            print("=> loading checkpoint... ")
+            checkpoint = torch.load('last_brain.pth')
+            self.qnetwork_local.load_state_dict(checkpoint['state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+            print("done !")
+        else:
+            print("no checkpoint found...")
 
 
 class ReplayBuffer:
